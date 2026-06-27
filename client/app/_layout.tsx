e4ff904ec3abd44508f8782a11930771f4b48b45
boundy99 <abdoulayeboundydjikine@gmail.com>
2026-06-27 19:19:07 -0400

    chore: boiler plate v2

---
+++ client/app/_layout.tsx
@@ -0,0 +1,6 @@
+import {Stack} from "expo-router";
+import "./public/style.css";
+
+export default function RootLayout() {
+  return <Stack />;
+}
--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,14 +1,12 @@
-import {StyleSheet, Text, View} from "react-native";
+import {Text, View} from "react-native";
 import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>This is Home</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-lg font-bold text-red-200">This is Home</Text>
     </View>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({});
